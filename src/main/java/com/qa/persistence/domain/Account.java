0003180d54554191ab9b9848bb2291aaf235154f
Admin <Admin@STUDENT10>
2019-01-31 09:47:49 +0000

    Added beans.xml, persistence.xml, pom.xml, account-ds.xml

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,11 +1,22 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Account {
 	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	@Column(length = 100)
 	private String firstName;
+	@Column(length = 100)
 	private String lastName;
+	@Column(length = 100)
 	private int accountNumber;
-	
+
 	public Account(String firstName, String lastName, int accountNumber){
 		
 		this.firstName = firstName;
